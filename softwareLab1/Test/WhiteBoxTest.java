7d53f08fa224730f00f13bf4e400dae2e5c7d721
zc <zc731018zyd@163.com>
2024-06-16 14:59:04 +0800

    third commit

--- softwareLab1/Test/WhiteBoxTest.java
+++ softwareLab1/Test/WhiteBoxTest.java
@@ -1,11 +1,11 @@
-import org.junit.jupiter.api.Test;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class TextToGraphTest {
+public class WhiteBoxTest {
 //    @BeforeClass
 //    public static void BeforeAll() {
     TextToGraph textToGraph = new TextToGraph();
@@ -15,21 +15,21 @@ class TextToGraphTest {
     Map<String, Map<String, Integer>> graph = textToGraph.buildGraph(words);
 //    }
     @Test
-    void calcShortestPath1() {
+    public void calcShortestPath1() {
         String startWord = "";
         String test = textToGraph.calcShortestPath(graph,startWord, "new");
 
         assertEquals("No " + startWord + " in the graph!",test);
     }
     @Test
-    void calcShortestPath2() {
+    public void calcShortestPath2() {
         String startWord = "to";
         String endWord = "and";
         String test = textToGraph.calcShortestPath(graph,startWord, endWord);
         assertEquals("Shortest path from to to and: to -> explore -> strange -> new -> life -> and [Total weight: 5]",test);
     }
     @Test
-    void calcShortestPath3() {
+    public void calcShortestPath3() {
         String startWord = "to";
         String endWord = "";
         String test = textToGraph.calcShortestPath(graph,startWord, endWord);
@@ -37,7 +37,7 @@ class TextToGraphTest {
         assertEquals("Shortest path from to to new: to -> explore -> strange -> new [Total weight: 3]\nShortest path from to to worlds: to -> explore -> strange -> new -> worlds [Total weight: 4]\nShortest path from to to explore: to -> explore [Total weight: 1]\nShortest path from to to and: to -> explore -> strange -> new -> life -> and [Total weight: 5]\nShortest path from to to civilizations: to -> explore -> strange -> new -> civilizations [Total weight: 4]\nShortest path from to to seek: to -> seek [Total weight: 1]\nShortest path from to to strange: to -> explore -> strange [Total weight: 2]\nShortest path from to to life: to -> explore -> strange -> new -> life [Total weight: 4]\nShortest path from to to out: to -> seek -> out [Total weight: 2]\n",test);
     }
     @Test
-    void calcShortestPath4() {
+    public void calcShortestPath4() {
         String startWord = "civilizations";
         String endWord = "new";
         String test = textToGraph.calcShortestPath(graph,startWord, endWord);
